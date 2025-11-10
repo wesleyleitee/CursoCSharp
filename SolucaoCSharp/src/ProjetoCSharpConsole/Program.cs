42353863a0069ad87dd8f1fd8a2141442e9a84fc
Wesley Leite <leitewesleyleite@gmail.com>
2025-11-10 06:04:47 -0300

    Exemplo de classe.

--- SolucaoCSharp/src/ProjetoCSharpConsole/Program.cs
+++ SolucaoCSharp/src/ProjetoCSharpConsole/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Pessoa p = new Pessoa();
+p.Nome = "Joaquim";
+p.Idade = 18;
+p.Apresentar();
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+
+    public void Apresentar()
+    {
+        Console.WriteLine($"Olá meu nome é {Nome}, e tenho {Idade} anos ");
+    }
+}
+
+
