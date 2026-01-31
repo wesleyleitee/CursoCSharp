85e844130a9cd019ee63f22a5ce43f02e6054ac2
Wesley Leite <leitewesleyleite@gmail.com>
2026-01-31 15:12:56 -0300

    Estruturas.

---
+++ SolucaoCSharp/src/Estruturas/Program.cs
@@ -0,0 +1,50 @@
+﻿
+//CarroClasse carroClasse = new CarroClasse();
+//carroClasse.Marca = "VW";
+//carroClasse.Ano = 2027;
+//carroClasse.Denominacao();
+
+//CarroClasse carro2 = carroClasse;
+//carro2.Marca = "GM";
+//carro2.Ano = 2028;
+//carro2.Denominacao();
+
+//carroClasse.Denominacao();
+
+//------------------------------------------
+
+CarroEstrutura carroEstrutura = new CarroEstrutura();
+carroEstrutura.Marca = "VW";
+carroEstrutura.Ano = 2029;
+carroEstrutura.Denominacao();
+
+CarroEstrutura carroEstrutura2 = carroEstrutura;
+carroEstrutura2.Marca = "Peugeot";
+carroEstrutura2.Ano = 2030;
+
+carroEstrutura2.Denominacao();
+carroEstrutura.Denominacao();
+
+Console.ReadKey();
+
+
+public class CarroClasse()
+{
+    public string Marca { get; set; }
+    public int Ano { get; set; }
+
+    public void Denominacao()
+    {
+        Console.WriteLine($"Classe - Carro da marca {Marca} e ano {Ano}");
+    }
+}
+
+public struct CarroEstrutura()
+{
+    public string Marca { get; set; }
+    public int Ano { get; set; }
+    public void Denominacao()
+    {
+        Console.WriteLine($"Estrutura - Carro da marca {Marca} e ano {Ano}");
+    }
+}
